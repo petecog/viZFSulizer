0011e62720209b4af1694515b8fb5535e44a4451
Peter Coghill <peter@alephinsights.com>
2025-02-23 07:13:18 +0000

    basic test framework added to support GH actions

---
+++ internal/tui/tui_test.go
@@ -0,0 +1,13 @@
+package tui
+
+import (
+	"testing"
+)
+
+func TestBasicFunctionality(t *testing.T) {
+	// Add basic tests for your TUI components
+	// Example:
+	if 1+1 != 2 {
+		t.Errorf("Basic math failed")
+	}
+}
